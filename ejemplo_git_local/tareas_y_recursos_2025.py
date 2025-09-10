bc93f701d33bbd2680f441dba9d3598ec842dfb3
Joan Bono Frigols <joanbonoprog@gmail.com>
2025-09-10 20:19:22 +0200

    Crear el archivo tareas_y_recursos_2025.py con la lista de tareas, recursos y objetivos para 2025

---
+++ ejemplo_git_local/tareas_y_recursos_2025.py
@@ -0,0 +1,8 @@
+def main(): 
+    print("Hola, Equipo!")
+    print("Este es la lista de cosas que hacer en 2025.")
+    print(mylist = ["hacer la compra", "desarrollar app de correo", "Hacer un colacao", "Tirar la basura", "Hacer monográfico de pokémon", "Diseño de interfaz"])
+    print("Este es la lista de recursos.")
+    print(mylist = ["2 coches", "1 oficina", "4 mesas", "2 portátiles"])
+    print("Este es la lista de objetivos.")          
+    print(mylist = ["Buscar capital", "Preparar clases antes del lunes", "Sacar matrícula de honor"])
